126d2f3e23750caf0b3ad6d8d9a22ee8ec092684
Max <zeetreex@gmail.com>
2023-03-03 21:04:54 +0200

    init

---
+++ server/src/main/kotlin/com/ecommerce/demo/GrandleemonApplication.kt
@@ -0,0 +1,11 @@
+package com.ecommerce.demo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GrandleemonApplication
+
+fun main(args: Array<String>) {
+	runApplication<GrandleemonApplication>(*args)
+}
