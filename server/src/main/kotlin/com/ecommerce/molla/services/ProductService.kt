591590dd2dbe00319d9a05a1ea8cc35fc9d16109
Max <zeetreex@gmail.com>
2023-03-05 13:28:51 +0200

    deleted folder

--- server/src/main/kotlin/com/ecommerce/molla/services/ProductService.kt
+++ server/src/main/kotlin/com/ecommerce/molla/services/ProductService.kt
@@ -7,8 +7,6 @@ import com.ecommerce.molla.repositories.ProductRepository
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Service
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.RequestBody
 import java.util.*
 
 @Service
